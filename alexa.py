88665496b16038638004b48b5b295362a6557870
Akhyar <ayozzet@gmail.com>
2020-12-24 01:23:22 +0800

    uncomplete

--- alexa.py
+++ alexa.py
@@ -19,42 +19,40 @@ def talk(text):
 def take_command():
     try:
         with sr.Microphone() as source:
-            print('listening...')
+            print("listening...")
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
+            if "alexa" in command:
+                command = command.replace("alexa", '')
                 print(command)
     except:
         pass
     return command
 
-
 def run_alexa():
     command = take_command()
     print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
+    if "play" in command:
+        song = command.replace("play", "")
         talk('playing ' + song)
         pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
+    elif "time" in command:
+        time = datetime.datetime.now().strftime("%I:%M %p")
+        talk("Current time is " + time)
+    elif "who the heck is" in command:
+        person = command.replace("who the heck is", "")
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
+    elif "date" in command:
+        talk("sorry, I have a headache")
+    elif "are you single" in command:
+        talk("I am in a relationship with wifi")
+    elif "joke" in command:
         talk(pyjokes.get_joke())
     else:
-        talk('Please say the command again.')
-
+        talk("Please say the command again.")
 
 while True:
     run_alexa()
